559ec0e2d2734b5085d4cde05ef3db50c762cfdf
Bruno M. Mergen <brunomergen@gmail.com>
2015-10-14 19:27:21 -0300

    Possibilitando a adição direta do certificate e do certificate_key para assinatura, sem a necessidade de utilizar o OpenSSL::PKCS12 para ler o certificado

--- lib/br_nfe/version.rb
+++ lib/br_nfe/version.rb
@@ -1,8 +1,8 @@
 module BrNfe
 	module Version
 		MAJOR = 1 #inclui alterações de API e pode quebrar compatibilidade com versões anteriores
-		MINOR = 0 #inclui novas funcionalidades, sem quebrar APIs existentes
-		PATCH = 2 #corrige bugs ou traz melhorias em implementações já existentes
+		MINOR = 1 #inclui novas funcionalidades, sem quebrar APIs existentes
+		PATCH = 0 #corrige bugs ou traz melhorias em implementações já existentes
 		CURRENT = "#{MAJOR}.#{MINOR}.#{PATCH}"
 	end
 end
