9dad2a606a51482d6bf1ccc692facee4f2532185
Bruno M. Mergen <brunomergen@gmail.com>
2016-06-30 19:27:48 -0300

    Identação

--- lib/br_nfe/service/sc/florianopolis/cancellation.rb
+++ lib/br_nfe/service/sc/florianopolis/cancellation.rb
@@ -17,8 +17,8 @@ module BrNfe
 					attr_accessor :serie_number
 					
 					validates :serie_number, presence: true
-					validates :motive,              length: {maximum: 120}, allow_blank: true
-					validates :serie_number,        numericality: {only_integer: true, less_than_or_equal_to: 999999},  allow_blank: true
+					validates :motive,       length:       {maximum: 120}, allow_blank: true
+					validates :serie_number, numericality: {only_integer: true, less_than_or_equal_to: 999999},  allow_blank: true
 
 					def xml_builder
 						render_xml 'cancellation', dir_path: "#{BrNfe.root}/lib/br_nfe/service/sc/florianopolis/xml"
