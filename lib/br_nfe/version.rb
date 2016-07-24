722fb0b4f5af63b1145fd104d56d9ada5a90159d
Bruno M. Mergen <brunomergen@gmail.com>
2016-07-24 00:49:30 -0300

    Update version to 2.0.0

--- lib/br_nfe/version.rb
+++ lib/br_nfe/version.rb
@@ -1,7 +1,7 @@
 module BrNfe
 	module Version
-		MAJOR = 1 #inclui alterações de API e pode quebrar compatibilidade com versões anteriores
-		MINOR = 1 #inclui novas funcionalidades, sem quebrar APIs existentes
+		MAJOR = 2 #inclui alterações de API e pode quebrar compatibilidade com versões anteriores
+		MINOR = 0 #inclui novas funcionalidades, sem quebrar APIs existentes
 		PATCH = 0 #corrige bugs ou traz melhorias em implementações já existentes
 		CURRENT = "#{MAJOR}.#{MINOR}.#{PATCH}"
 	end
