aa49aba84fdd78f6538e980b4465005f17be81d9
Bruno M. Mergen <brunomergen@gmail.com>
2016-09-19 19:13:51 -0300

    #4 - Adicionado arquivos XSD para validação do schema e validando consulta_status_servico via schema XSD

--- test/br_nfe/product/consulta_status_servico_test.rb
+++ test/br_nfe/product/consulta_status_servico_test.rb
@@ -35,4 +35,25 @@ describe BrNfe::Product::ConsultaStatusServico do
 		end
 	end
 
+	describe "Validação do XML através do XSD" do		
+		let(:schemas_dir) { BrNfe.root+'/lib/br_nfe/product/xml/v3_10/XSD' }		
+		def validate_schema
+			subject.stubs(:certificate).returns(nil)			
+			Dir.chdir(schemas_dir) do
+				schema = Nokogiri::XML::Schema(IO.read('consStatServ_v3.10.xsd'))
+				document = Nokogiri::XML(subject.xml_builder)
+				errors = schema.validate(document)
+				errors.must_be_empty
+			end
+		end
+		it "Deve ser válido em ambiente de produção" do
+			subject.env = :production
+			validate_schema
+		end
+		it "Deve ser válido em ambiente de homologação" do
+			subject.env = :test
+			validate_schema
+		end
+	end
+
 end
