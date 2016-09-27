854a569d53f15a3b8951ad4976d72930252108ea
Bruno M. Mergen <brunomergen@gmail.com>
2016-09-27 18:34:25 -0300

    #4 - Criado configuração do Web Service do SVRS

--- lib/br_nfe/product/gateway/web_service_svrs.rb
+++ lib/br_nfe/product/gateway/web_service_svrs.rb
@@ -24,15 +24,98 @@ module BrNfe
 
 				##########################################################################################
 				################################  NFE AUTORIZAÇÃO  #######################################
-					# def wsdl_autorizacao
-						# 'https://nfe.svrs.rs.gov.br/ws/NfeAutorizacao/NFeAutorizacao.asmx?wsdl'
-					# end
-					# def operation_autorizacao
-						# :nfe_autorizacao_lote
-					# end
-					# def version_xml_autorizacao
-						# :v3_10
-					# end
+					def wsdl_autorizacao
+						if env_production?
+							'https://nfe.svrs.rs.gov.br/ws/NfeAutorizacao/NFeAutorizacao.asmx?wsdl'
+						else
+							'https://nfe-homologacao.svrs.rs.gov.br/ws/NfeAutorizacao/NFeAutorizacao.asmx?wsdl'
+						end
+					end
+					def operation_autorizacao
+						:nfe_autorizacao_lote
+					end
+					def version_xml_autorizacao
+						:v3_10
+					end
+					def url_xmlns_autorizacao
+						'http://www.portalfiscal.inf.br/nfe/wsdl/NfeAutorizacao'
+					end
+
+				##########################################################################################
+				################################  NFE RETORNO AUTORIZAÇÃO  ###############################
+					def wsdl_retorno_autorizacao
+						if env_production?
+							'https://nfe.svrs.rs.gov.br/ws/NfeRetAutorizacao/NFeRetAutorizacao.asmx?wsdl'
+						else
+							'https://nfe-homologacao.svrs.rs.gov.br/ws/NfeRetAutorizacao/NFeRetAutorizacao.asmx?wsdl'
+						end
+					end
+					def operation_retorno_autorizacao
+						:nfe_ret_autorizacao_lote
+					end
+					def version_xml_retorno_autorizacao
+						:v3_10
+					end
+					def url_xmlns_retorno_autorizacao
+						'http://www.portalfiscal.inf.br/nfe/wsdl/NfeRetAutorizacao'
+					end
+
+				##########################################################################################
+				################################  NFE CONSULTA PROTOCOLO  ################################
+					def wsdl_consulta_protocolo
+						if env_production?
+							'https://nfe.svrs.rs.gov.br/ws/NfeConsulta/NfeConsulta2.asmx?wsdl'
+						else
+							'https://nfe-homologacao.svrs.rs.gov.br/ws/NfeConsulta/NfeConsulta2.asmx?wsdl'
+						end
+					end
+					def operation_consulta_protocolo
+						:nfe_consulta_nf2
+					end
+					def version_xml_consulta_protocolo
+						:v3_10
+					end
+					def url_xmlns_consulta_protocolo
+						'http://www.portalfiscal.inf.br/nfe/wsdl/NfeConsulta2'
+					end
+
+				##########################################################################################
+				################################  NFE INUTILIZAÇÃO  ######################################
+					def wsdl_inutilizacao
+						if env_production?
+							'https://nfe.svrs.rs.gov.br/ws/nfeinutilizacao/nfeinutilizacao2.asmx?wsdl'
+						else
+							'https://nfe-homologacao.svrs.rs.gov.br/ws/nfeinutilizacao/nfeinutilizacao2.asmx?wsdl'
+						end
+					end
+					def operation_inutilizacao
+						:nfe_inutilizacao_nf2
+					end
+					def version_xml_inutilizacao
+						:v3_10
+					end
+					def url_xmlns_inutilizacao
+						'http://www.portalfiscal.inf.br/nfe/wsdl/NfeInutilizacao2'
+					end
+
+				##########################################################################################
+				################################  NFE RECEPÇÃO EVENTO  ###################################
+					def wsdl_recepcao_evento
+						if env_production?
+							'https://nfe.svrs.rs.gov.br/ws/recepcaoevento/recepcaoevento.asmx?wsdl'
+						else
+							'https://nfe-homologacao.svrs.rs.gov.br/ws/recepcaoevento/recepcaoevento.asmx?wsdl'
+						end
+					end
+					def operation_recepcao_evento
+						:nfe_recepcao_evento
+					end
+					def version_xml_recepcao_evento
+						:v1_00
+					end
+					def url_xmlns_recepcao_evento
+						'http://www.portalfiscal.inf.br/nfe/wsdl/RecepcaoEvento'
+					end
 			end
 		end
 	end
--- test/br_nfe/product/gateway/web_service_svrs_test.rb
+++ test/br_nfe/product/gateway/web_service_svrs_test.rb
@@ -31,6 +31,131 @@ describe BrNfe::Product::Gateway::WebServiceSVRS do
 			it { subject.url_xmlns_status_servico.must_equal 'http://www.portalfiscal.inf.br/nfe/wsdl/NfeStatusServico2' }
 		end		
 	end
+
+	describe 'AUTORIZAÇÃO' do
+		describe '#wsdl_autorizacao' do
+			it "para ambiente de produção" do
+				subject.env = :production
+				subject.wsdl_autorizacao.must_equal 'https://nfe.svrs.rs.gov.br/ws/NfeAutorizacao/NFeAutorizacao.asmx?wsdl'
+			end
+			it "para ambiente de homologação" do
+				subject.env = :test
+				subject.wsdl_autorizacao.must_equal 'https://nfe-homologacao.svrs.rs.gov.br/ws/NfeAutorizacao/NFeAutorizacao.asmx?wsdl'
+			end
+		end
+
+		describe '#operation_autorizacao' do
+			it { subject.operation_autorizacao.must_equal :nfe_autorizacao_lote }
+		end
+
+		describe '#version_xml_autorizacao' do
+			it { subject.version_xml_autorizacao.must_equal :v3_10 }
+		end
+
+		describe '#url_xmlns_autorizacao' do
+			it { subject.url_xmlns_autorizacao.must_equal 'http://www.portalfiscal.inf.br/nfe/wsdl/NfeAutorizacao' }
+		end		
+	end
+
+	describe 'RETORNO AUTORIZAÇÃO' do
+		describe '#wsdl_retorno_autorizacao' do
+			it "para ambiente de produção" do
+				subject.env = :production
+				subject.wsdl_retorno_autorizacao.must_equal 'https://nfe.svrs.rs.gov.br/ws/NfeRetAutorizacao/NFeRetAutorizacao.asmx?wsdl'
+			end
+			it "para ambiente de homologação" do
+				subject.env = :test
+				subject.wsdl_retorno_autorizacao.must_equal 'https://nfe-homologacao.svrs.rs.gov.br/ws/NfeRetAutorizacao/NFeRetAutorizacao.asmx?wsdl'
+			end
+		end
+
+		describe '#operation_retorno_autorizacao' do
+			it { subject.operation_retorno_autorizacao.must_equal :nfe_ret_autorizacao_lote }
+		end
+
+		describe '#version_xml_retorno_autorizacao' do
+			it { subject.version_xml_retorno_autorizacao.must_equal :v3_10 }
+		end
+
+		describe '#url_xmlns_retorno_autorizacao' do
+			it { subject.url_xmlns_retorno_autorizacao.must_equal 'http://www.portalfiscal.inf.br/nfe/wsdl/NfeRetAutorizacao' }
+		end
+	end
+
+	describe 'CONSULTA PROTOCOLO' do
+		describe '#wsdl_consulta_protocolo' do
+			it "para ambiente de produção" do
+				subject.env = :production
+				subject.wsdl_consulta_protocolo.must_equal 'https://nfe.svrs.rs.gov.br/ws/NfeConsulta/NfeConsulta2.asmx?wsdl'
+			end
+			it "para ambiente de homologação" do
+				subject.env = :test
+				subject.wsdl_consulta_protocolo.must_equal 'https://nfe-homologacao.svrs.rs.gov.br/ws/NfeConsulta/NfeConsulta2.asmx?wsdl'
+			end
+		end
+
+		describe '#operation_consulta_protocolo' do
+			it { subject.operation_consulta_protocolo.must_equal :nfe_consulta_nf2 }
+		end
+
+		describe '#version_xml_consulta_protocolo' do
+			it { subject.version_xml_consulta_protocolo.must_equal :v3_10 }
+		end
+
+		describe '#url_xmlns_consulta_protocolo' do
+			it { subject.url_xmlns_consulta_protocolo.must_equal 'http://www.portalfiscal.inf.br/nfe/wsdl/NfeConsulta2' }
+		end
+	end
+	
+	describe 'NFE INUTILIZAÇÃO' do
+		describe '#wsdl_inutilizacao' do
+			it "para ambiente de produção" do
+				subject.env = :production
+				subject.wsdl_inutilizacao.must_equal 'https://nfe.svrs.rs.gov.br/ws/nfeinutilizacao/nfeinutilizacao2.asmx?wsdl'
+			end
+			it "para ambiente de homologação" do
+				subject.env = :test
+				subject.wsdl_inutilizacao.must_equal 'https://nfe-homologacao.svrs.rs.gov.br/ws/nfeinutilizacao/nfeinutilizacao2.asmx?wsdl'
+			end
+		end
+
+		describe '#operation_inutilizacao' do
+			it { subject.operation_inutilizacao.must_equal :nfe_inutilizacao_nf2 }
+		end
+
+		describe '#version_xml_inutilizacao' do
+			it { subject.version_xml_inutilizacao.must_equal :v3_10 }
+		end
+
+		describe '#url_xmlns_inutilizacao' do
+			it { subject.url_xmlns_inutilizacao.must_equal 'http://www.portalfiscal.inf.br/nfe/wsdl/NfeInutilizacao2' }
+		end
+	end
+	
+	describe 'RECEPÇÃO EVENTO' do
+		describe '#wsdl_recepcao_evento' do
+			it "para ambiente de produção" do
+				subject.env = :production
+				subject.wsdl_recepcao_evento.must_equal 'https://nfe.svrs.rs.gov.br/ws/recepcaoevento/recepcaoevento.asmx?wsdl'
+			end
+			it "para ambiente de homologação" do
+				subject.env = :test
+				subject.wsdl_recepcao_evento.must_equal 'https://nfe-homologacao.svrs.rs.gov.br/ws/recepcaoevento/recepcaoevento.asmx?wsdl'
+			end
+		end
+
+		describe '#operation_recepcao_evento' do
+			it { subject.operation_recepcao_evento.must_equal :nfe_recepcao_evento }
+		end
+
+		describe '#version_xml_recepcao_evento' do
+			it { subject.version_xml_recepcao_evento.must_equal :v1_00 }
+		end
+
+		describe '#url_xmlns_recepcao_evento' do
+			it { subject.url_xmlns_recepcao_evento.must_equal 'http://www.portalfiscal.inf.br/nfe/wsdl/RecepcaoEvento' }
+		end
+	end
 	
 	
 end
